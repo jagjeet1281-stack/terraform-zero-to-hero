0eb0870db213fc85e650acff49b19a4cada47266
Jagjeet1281-stack <jagjeet1281@gmail.com>
2026-01-30 00:02:23 +0800

    added

--- Jeet_test/variables.tf
+++ Jeet_test/variables.tf
@@ -1,17 +1,14 @@
 variable "ami_value" {
   description = "The AMI ID for the EC2 instance"
-  type        = string  
   
 }
 
 variable "instance_type_value"  {
-    description = "The instance type for the EC2 instance"
-    type        = string    
+    description = "The instance type for the EC2 instance"  
   
 }
 
 variable "subnet_id_value" {
-    description = "The Subnet ID for the EC2 instance"
-    type        = string    
+    description = "The Subnet ID for the EC2 instance"  
   
 }
