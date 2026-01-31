db59a4e3b06beb68e0fb8a0f766c4f02096bbcac
Jagjeet1281-stack <jagjeet1281@gmail.com>
2026-01-31 13:58:23 +0800

    added

--- Jeet_test/backend.tf
+++ Jeet_test/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "demo-terraform-eks-state-s3-bucket-1291 "
+    bucket = "demo-terraform-eks-state-s3-bucket-1291"
     region = "us-west-2"
     key = "terraform.tfstate"
     
